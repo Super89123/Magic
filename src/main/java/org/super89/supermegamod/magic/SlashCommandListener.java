617e56cf62a5f3e8433b58b63e13883c50600256
Ilya <tedswa@mail.ru>
2024-12-11 21:51:33 +0400

    Рабочий дискорд

--- src/main/java/org/super89/supermegamod/magic/SlashCommandListener.java
+++ src/main/java/org/super89/supermegamod/magic/SlashCommandListener.java
@@ -1,11 +1,15 @@
 package org.super89.supermegamod.magic;
 
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
+
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.checkerframework.framework.qual.QualifierArgument;
 
 public class SlashCommandListener extends ListenerAdapter {
     @Override
@@ -13,21 +17,32 @@ public class SlashCommandListener extends ListenerAdapter {
     {
         System.out.println("I showes smt");
         event.getChannel().sendMessage("ffff");
-        // Only accept commands from guilds
+
         if (event.getGuild() == null)
             return;
         switch (event.getName())
         {
 
             case "players":
-                say(event, Bukkit.getOnlinePlayers().toString());
-                event.getChannel().sendMessage((CharSequence) Bukkit.getOnlinePlayers().toString());
-                System.out.println("I do it");// content is required so no null-check here
+                event.deferReply().queue();
+                EmbedBuilder info = new EmbedBuilder();
+                info.setTitle("Игроки в сети");
+                info.setDescription("Играют:");
+                info.setColor(0xf45642);
+                info.setFooter("Quantov", event.getMember().getUser().getAvatarUrl());
+                for(Player player : Bukkit.getOnlinePlayers()){
+                     // event.getHook().sendMessage(player.getName()).queue();
+                    //event.getHook().sendMessage(player.getAddress().getHostString()).queue();
+                    info.addField(player.getName(), player.getName()+" "+player.getAddress().getHostString(), true);
+                }
+                event.getHook().sendMessageEmbeds(info.build()).queue();
+                System.out.println("I do it");
                 break;
             default:
                 event.reply("I can't handle that command right now :(").setEphemeral(true).queue();
         }
     }
+
     @Override
     public void onReady(ReadyEvent event){
         System.out.println("я вкл");
